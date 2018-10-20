618467b63c01f8c37e0995c2791dbf1f5bcb7446
Prashant Chaudhari <prashantchaudhari881@gmail.com>
2018-10-20 14:22:09 +0530

    Regex explaination and HTTP method parsing.
    Added Comment explaining the **Regex**
    used in `.replace` method.
    
    Code can now also output the **method**
    used for requests.

--- index.js
+++ index.js
@@ -13,14 +13,21 @@ const server = http.createServer((req, res) => {
   const parsedURL = url.parse(req.url, true);
 
   // get the path
+
+  /*
+  * * The Regex in .replace method means that, look for one or more slash symbol at the start of the string and at the end of the string and then replace them with empty string.
+  */
   const path = parsedURL.pathname;
   const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+  // get the http method
+  const method = req.method.toUpperCase();
 
   // send the response
   res.end('Hello World\n');
 
   // log the request path
-  console.log(`Request received on path : ${trimmedPath}`);
+  console.log(`Request received on path : ${trimmedPath} with the ${method} method.`);
   
 });
 
